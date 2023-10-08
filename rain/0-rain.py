29732a11c8dbc97f66f0607f97e182fa7ff84d06
Maxime-Bakunzi <m.bakunzi@alustudent.com>
2023-10-08 21:57:40 +0000

    Adding The Code$

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
+
+"""
+rain.py
+"""
+
+
 def rain(walls):
     """
     Calculate how many square units of water will be retained after it rains.
 
     Args:
-        walls (list): A list of non-negative integers representing the heights of walls.
+        walls (list): List of non-negative integers representing the heights of walls.
 
     Returns:
-        int: The total amount of rainwater retained.
-
+        int: Total amount of rainwater retained.
     """
     if len(walls) == 0:
         return 0
@@ -18,17 +24,16 @@ def rain(walls):
 
     left_max[0] = walls[0]
     for i in range(1, len(walls)):
-        left_max[i] = max(left_max[i-1], walls[i])
+        left_max[i] = max(left_max[i - 1], walls[i])
 
     right_max[-1] = walls[-1]
-    for i in range(len(walls)-2, -1, -1):
-        right_max[i] = max(right_max[i+1], walls[i])
+    for i in range(len(walls) - 2, -1, -1):
+        right_max[i] = max(right_max[i + 1], walls[i])
 
     total_water = 0
     for i in range(len(walls)):
-        water_level = min(left_max[i], right_max[i])
-        if water_level > walls[i]:
-            total_water += water_level - walls[i]
+        water_height = min(left_max[i], right_max[i]) - walls[i]
+        total_water += water_height
 
     return total_water
 
